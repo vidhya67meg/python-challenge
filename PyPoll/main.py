2a201953f33a72adc407a8c2cc0481da4a229a1d
vidhya67meg <mvidhya.ft@gmail.com>
2023-05-01 16:25:42 -0700

    added comments

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,54 +1,56 @@
 import os
+# Module for reading CSV files
 import csv
-
 csvpath = os.path.join('Resources','election_data.csv')
-
-# def vote_percentage (voting_data):
-#     ballot_id = voting_data[0]
-#     county = voting_data[1]
-#     candidate = voting_data[2]
-
     
-
+# Declare variables
 total_votes = 0
 candidate_votes = {}
+max_vote = 0
+max_vote_name = str
+
+# CSV reader specifies delimiter and variable that holds contents
 with open (csvpath) as csvfile :
     csvreader = csv.reader(csvfile, delimiter = ',')
-
+    # Read the header row first
     csv_header = next(csvreader)
 
-    
+    # Read each row of data after the header   
     for row in csvreader:
-    
         total_votes = total_votes + 1
+        # assigning votes to unique candidates
         candidate_votes[row[2]] = candidate_votes.get(row[2], 0) + 1
     
         
-max_vote = 0
-max_vote_name = str
+
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {total_votes}")
 print("-------------------------")
+# Printing the unique candidates determined, with their respective votes and percentage of votes
 for candidate in candidate_votes:
-    print(f"{candidate}: {round((candidate_votes[candidate]/total_votes)*100,3)}% ({candidate_votes[candidate]})")
-    if candidate_votes[candidate] > max_vote:
-        max_vote = candidate_votes[candidate]
+    vote = candidate_votes[candidate]
+    print(f"{candidate}: {round((vote/total_votes)*100,3)}% ({vote})")
+    # Determining winner by declaring variables and finding maximum value in unique candidate list
+    if  vote > max_vote:
+        max_vote = vote
         max_vote_name = candidate
 print("-------------------------")
 print(f"Winner: {max_vote_name}")
 print("-------------------------")
   
-
+# Specify the file to write to
 txtpath = os.path.join('analysis','results.txt')
-
+# Open the file using "write" mode. 
 with open(txtpath, 'w') as f:
+    # Write the results
     f.write("Election Results\n")
     f.write("----------------------------\n")
     f.write(f"Total Votes: {total_votes}\n")
     f.write("----------------------------\n")
     for candidate in candidate_votes:
-         f.write(f"{candidate}: {round((candidate_votes[candidate]/total_votes)*100,3)}% ({candidate_votes[candidate]})\n")
+        vote = candidate_votes[candidate]
+        f.write(f"{candidate}: {round((vote/total_votes)*100,3)}% ({vote})\n")
     f.write("-------------------------\n")
     f.write(f"Winner: {max_vote_name}\n")
     f.write("-------------------------\n")
