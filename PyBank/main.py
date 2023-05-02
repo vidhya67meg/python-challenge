1471237ff15c07d4104fbed7f7c773c022e346a9
vidhya67meg <mvidhya.ft@gmail.com>
2023-05-01 17:47:06 -0700

    comments added

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,7 +12,7 @@ with open(csvpath) as csvfile:
     # Read the header row  
     csv_header = next(csvreader)
 
-
+    # declare the variables
     count = 0
     total_amount = 0
     change = 0
@@ -31,9 +31,11 @@ with open(csvpath) as csvfile:
        count = count + 1
        # total amount of "profit/losses"
        total_amount = total_amount + profit_loss
+       # Determine change by comparing current and previous value and adding to overall change
        change = profit_loss - previous_value
        overall_change = overall_change + change
        previous_value = profit_loss
+       # determing greatest increase and decrease 
        if change > greatest_increase:
            greatest_increase = change
            greatest_month = row[0]
@@ -42,7 +44,7 @@ with open(csvpath) as csvfile:
            greatest_decrease_month = row[0]
 
 
-
+    # Print results
     print("Financial Analysis")
     print("----------------------------")
     print(f"Total Months: {count}")
@@ -51,9 +53,9 @@ with open(csvpath) as csvfile:
     print(f"Greatest Increase in Profits: {greatest_month} (${greatest_increase})")
     print(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})")
 
-
+# Specify the file to write to
 txtpath = os.path.join('analysis','results.txt')
-
+# Write to text file
 with open(txtpath, 'w') as f:
     f.write("Financial Analysis\n")
     f.write("----------------------------\n")
